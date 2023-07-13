4c1310b806efcb890f1ef5deb0569b0d9c20a083
MarkNzioka <markkivuva7@gmail.com>
2023-07-13 12:30:02 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - begin code
+ * Return: 1 always
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
