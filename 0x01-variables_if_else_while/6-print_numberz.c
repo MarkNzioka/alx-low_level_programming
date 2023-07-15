c001e48f3f81785257790b360c03008c7fea3f75
MarkNzioka <markkivuva7@gmail.com>
2023-07-14 20:15:55 -0700

    Numberz in ASCII

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - intro of code
+ * Return: 0 always
+ */
+int main(void)
+{
+	int l;
+
+	for (l = 0; l <= 9; l++)
+	{
+		putchar(l + 48);
+	}
+	putchar('\n');
+	return (0);
+}
