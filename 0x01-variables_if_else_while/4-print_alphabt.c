03c51786a813ca3ebd405c2da16310e9071cbf5a
MarkNzioka <markkivuva7@gmail.com>
2023-07-14 19:57:59 -0700

    Omit letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Introduction to the code
+ * Return: 0 always
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
