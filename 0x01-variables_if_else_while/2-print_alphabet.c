2b1cb517d422628716492b7bfd85ea71c634fd6a
MarkNzioka <markkivuva7@gmail.com>
2023-07-14 19:31:48 -0700

    Print alphabet in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - beginning of code
+ *Return: 0 always
+ */
+int main(void)
+{
+	int n = 97;
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
