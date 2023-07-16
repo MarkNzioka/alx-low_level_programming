fba9cff10a4a63ac123a9f43b9a32200d77e9382
MarkNzioka <markkivuva7@gmail.com>
2023-07-16 12:46:08 -0700

    Combine numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - intro to code
+ * Return: 0 always
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = '0'; a < '9'; a++)
+	{
+		for (b = a + 1; b <= '9'; b++)
+		{
+			if (a != b)
+			{
+				putchar(a);
+				putchar(b);
+				if (a == '8' && b == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
