7a637303486945344876b8fdfe1b3584fb911aa6
MarkNzioka <markkivuva7@gmail.com>
2023-07-15 20:40:51 -0700

    Base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - intro to code
+ * Return: 0 always
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+	for (b = 97; b <= 102; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
