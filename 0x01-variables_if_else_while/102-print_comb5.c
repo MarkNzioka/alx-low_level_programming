07114df7de49cff47b4520ce7d821bf84164ae12
MarkNzioka <markkivuva7@gmail.com>
2023-07-16 13:15:00 -0700

    Combine many numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - intro to the code
+ * Return: 0 always
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 98; a++)
+	{
+		for (b = a + 1; b <= 99; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a == 98 && b == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
