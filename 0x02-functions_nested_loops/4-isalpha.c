ad51478f4b7ac990a33e1ceb5fa30811c1a37d83
MarkNzioka <markkivuva7@gmail.com>
2023-07-18 12:39:17 -0700

    Check if it is an alphabet in lower or uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - check for alphabet
+ * @c: parameter to check
+ * Return: 1 if alphabet 0 if not
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	return (0);
+}
