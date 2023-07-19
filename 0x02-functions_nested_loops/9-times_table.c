7212cc677d6f21170d76302cc0ebd6faeaa11ae9
MarkNzioka <markkivuva7@gmail.com>
2023-07-19 08:57:21 -0700

    Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,28 +5,30 @@
  */
 void times_table(void)
 {
-	int a, b, c, d, e;
+	int a, b, c;
 
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-			if (c > 9)
+			if (b == 0)
 			{
-				d = c % 10;
-				e = (c - d) / 10;
-				_putchar(44);
-				_putchar(32);
-				_putchar(e + '0');
-				_putchar(d + '0');
-			}
-			else if (b != 0)
-			{
-				_putchar(44);
-				_putchar(32);
-				_putchar(32);
 				_putchar(c + '0');
+			}
+			if (c < 10 && b != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(c + '0');
+			}
+			else if (c >= 10)
+			{
+				_putchar(44);
+				_putchar(32);
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
 			}
 		}
 		_putchar('\n');
