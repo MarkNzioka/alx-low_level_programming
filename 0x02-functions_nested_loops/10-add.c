7343bc26ddb23e094edeb7e330c138c67d15c166
MarkNzioka <markkivuva7@gmail.com>
2023-07-19 08:46:21 -0700

    Sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - add two integers
+ * Return: sum
+ * @a: parameter to be checked
+ * @b: parameter to be checked
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
