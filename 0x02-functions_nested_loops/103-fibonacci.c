824cebc63a78a823d75bd9a6480abb07ce7a2fb3
MarkNzioka <markkivuva7@gmail.com>
2023-07-19 12:26:20 -0700

    Sum of fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ * main - sum of even fibonacci numbers
+ * Return: 0 always
+ */
+int main(void)
+{
+	unsigned long num1, num2, sum, sum_all;
+
+	num1 = 0;
+	num2 = 1;
+
+	while (1)
+	{
+		sum = num1 + num2;
+		if (sum > 4000000)
+			break;
+		if ((sum % 2) == 0)
+			sum_all = sum_all + sum;
+		num1 = num2;
+		num2 = sum;
+	}
+	printf("%lu\n", sum_all);
+	return (0);
+}
