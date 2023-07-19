aab5760cdd4a1ab7c78d96a25dcf14f531dedac3
MarkNzioka <markkivuva7@gmail.com>
2023-07-19 10:18:14 -0700

    Times tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -25,7 +25,7 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(k + '0');
 				}
-				else if (k >= 10 && k < 100)
+				else if (k >= 10 && k <= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
@@ -36,7 +36,6 @@ void print_times_table(int n)
 				else if (k > 100)
 				{
 					_putchar(',');
-					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
