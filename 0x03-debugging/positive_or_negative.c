97b77fb8a489b0bf91eced5e97476fc783fc0619
MarkNzioka <markkivuva7@gmail.com>
2023-07-20 07:44:21 -0700

    Check zero

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -6,9 +6,9 @@
 void positive_or_negative(int i)
 {
 	if (i > 0)
-		printf("%d is positive\n");
+		printf("%d is positive\n", i);
 	else if (i < 0)
-		printf("%d is negative\n");
+		printf("%d is negative\n", i);
 	else
-		printf("%d is zero\n");
+		printf("%d is zero\n", i);
 }
