f5e6239f318cff256f651942af3ef908005eee8a
MarkNzioka <markkivuva7@gmail.com>
2023-07-20 11:22:52 -0700

    Product

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - multiply 2 integers
+ * @a: parameter to be checked
+ * @b: parameter to be checked
+ * Return: product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
