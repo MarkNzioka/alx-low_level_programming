7476daeed895885be0d615b7ea115c2646e78fdc
MarkNzioka <markkivuva7@gmail.com>
2023-07-21 13:28:12 -0700

    Prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,4 +5,16 @@
  */
 int main(void)
 {
-	int a = 612852475143;
+	long int a = 612852475143, b;
+
+	for (b = 2; b <= a; b++)
+	{
+		if (a % b == 0)
+		{
+			a /= b;
+			b--;
+		}
+	}
+	printf("%ld\n", b);
+	return (0);
+}
