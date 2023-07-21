46ab840345c68d111029284e1f9c82485c6e9a76
MarkNzioka <markkivuva7@gmail.com>
2023-07-21 11:45:56 -0700

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,19 +12,23 @@ int main(void)
 	{
 		if (num % 3 == 0 && num % 5 == 0)
 		{
-			printf(" FizzBuzz");
+			printf("FizzBuzz ");
 		}
 		else if (num % 3 == 0)
 		{
-			printf(" Fizz");
+			printf("Fizz ");
 		}
 		else if (num % 5 == 0)
 		{
-			printf(" Buzz");
+			printf("Buzz ");
+		}
+		else if (num == 100)
+		{
+			printf("%d", num);
 		}
 		else
 		{
-			printf(" %d", num);
+			printf("%d ", num);
 		}
 	}
 	printf("\n");
