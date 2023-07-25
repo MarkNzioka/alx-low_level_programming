a96d60b656a374443b6db3efec30e18cc94fc9bb
MarkNzioka <markkivuva7@gmail.com>
2023-07-25 11:50:57 -0700

    Print half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+ * puts_half - print half of the string
+ * @str: string to print half of
+ */
+void puts_half(char *str)
+{
+	int xter = 0, i;
+
+	while (*str)
+	{
+		str++;
+		xter++;
+	}
+	for (i = 0; i < xter; i++)
+		str--;
+	i = (xter % 2 == 0) ? xter / 2 : (xter - 1) / 2;
+	for (; i < xter; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
