fcdf9ed8d772e09495963cf9bc19e926c16c965a
MarkNzioka <markkivuva7@gmail.com>
2023-07-28 14:52:29 -0700

    Buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+ * print_buffer - print buffer
+ * @size: content of buffer in bytes
+ * @b: pointer
+ * Return: 0 or a new line
+ */
+void print_buffer(char *b, int size)
+{
+
+}
