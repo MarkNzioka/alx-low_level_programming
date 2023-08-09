56b36ed6a53e30b6d44ee585d4b98364ceea9498
MarkNzioka <markkivuva7@gmail.com>
2023-08-09 09:59:57 -0700

    Concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,11 +10,10 @@ char *str_concat(char *s1, char *s2)
 	int a = 0, b = 0, length1 = 0, length2 = 0;
 	char *join;
 
-	if (s1[0] == '\0' || s2[0] == '\0')
-	{
+	if (s1 == NULL)
 		s1 = "";
+	if (s2 == NULL)
 		s2 = "";
-	}
 	while (s1[length1] != '\0')
 		length1++;
 	while (s2[length2] != '\0')
