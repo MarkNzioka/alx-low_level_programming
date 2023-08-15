e8bf5c8528bf270b1987d11d695579edb82ea43e
MarkNzioka <markkivuva7@gmail.com>
2023-08-15 14:06:15 -0700

    Print the x-tics of the dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,14 @@
+#include"dog.h"
+/**
+ * print_dog - print contents of struct dog
+ * @d: pointer to print
+ */
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		printf("Name: %s\n", d->name ? d->name : "(nill)");
+		printf("Age: %.6f\n", d->age);
+		printf("Owner: %s\n", d->owner ? d->owner : "(nill)");
+	}
+}
