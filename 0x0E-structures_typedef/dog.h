79b2f6e0094875bbb608db95f3b84822a5bc80ec
MarkNzioka <markkivuva7@gmail.com>
2023-08-15 13:23:23 -0700

    Dog header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+#include <stdio.h>
+
+/**
+ * struct dog - dog x-tics
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
