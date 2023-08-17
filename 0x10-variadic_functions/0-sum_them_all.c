ab03202f67263d84bb0c95994a8446d510c1763f
MarkNzioka <markkivuva7@gmail.com>
2023-08-17 13:55:18 -0700

    Sum all integers

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include"variadic_functions.h"
+/**
+ * sum_them_all - sum all integers in its parameter
+ * @n: integers to sum
+ * Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	unsigned int i;
+	va_list args;
+
+	if (n == 0)
+		return (0);
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
+	va_end(args);
+	return (sum);
+}
