06e312a25a94024dc209f1da7eef5cd875fabe87
MarkNzioka <markkivuva7@gmail.com>
2023-08-18 14:06:08 -0700

    Header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,11 @@
 #include<stdio.h>
 #include<stdarg.h>
 
+typedef struct FormatSpecifier
+{
+        char type;
+        void (*print_function)(va_list);
+} FormatSpecifier;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
