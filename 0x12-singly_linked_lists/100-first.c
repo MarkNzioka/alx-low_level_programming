ca8fb70cfcec1dc1b5d7924ba904b1c12ec1a769
MarkNzioka <markkivuva7@gmail.com>
2023-08-24 23:43:37 -0700

    Print before main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include"lists.h"
+/**
+ * print_before_main - print the message before main function
+ * is executed
+ */
+void __attribute__((constructor)) print_before_main()
+{
+	printf("You are beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
