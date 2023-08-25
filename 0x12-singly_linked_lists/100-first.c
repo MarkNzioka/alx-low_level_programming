5e4362b534fff43d4212eeb197eb6d31fe9f3bb9
MarkNzioka <markkivuva7@gmail.com>
2023-08-24 23:46:54 -0700

    Print before main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,6 +5,5 @@
  */
 void __attribute__((constructor)) print_before_main()
 {
-	printf("You are beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("You are beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
