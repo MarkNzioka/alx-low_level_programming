64dd71b9a8f241bde2dc0e1ace3c6a658f6fbc96
MarkNzioka <markkivuva7@gmail.com>
2023-08-30 12:54:10 -0700

    Insert a new node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new_node);
 	}
 	current = *head;
-	for (i = 0; i > idx - 1; i++)
+	for (i = 0; i < idx - 1; i++)
 	{
 		if (current == NULL)
 		{
