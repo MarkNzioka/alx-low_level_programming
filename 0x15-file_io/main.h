f1c906ec209dcb07aa0b651dada9165b0617d887
root <markkivuva7@gmail.com>
2023-10-03 15:03:27 -0700

    Header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,9 +6,16 @@
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdarg.h>
+
+#define BUFFER_SIZE 1024
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void exit_message(int code, const char *format, const char *arg);
+int main(int argc, char *argv[]);
 
 #endif
